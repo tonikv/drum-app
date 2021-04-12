dc6c403c90cf1584b6ffa12464bc61677123803e
Toni Virtanen <toniKoodaa@gmail.com>
2021-04-12 13:12:02 +0300

    Updates

---
+++ static/js/main.20116f98.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdrum-app"]=this["webpackJsonpdrum-app"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),i=t(3),s=t.n(i),a=(t(8),t(9),t(0));var p=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("h1",{children:"Drum machine"})})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(p,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.20116f98.chunk.js.map
