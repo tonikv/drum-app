433eb5b576b6188cb9c5aec8487fb7c1586e04f3
Toni Virtanen <toniKoodaa@gmail.com>
2021-04-13 11:19:13 +0300

    sound files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './App.css'
-import Qsound from './sounds/Q.wav'
-import Wsound from './sounds/W.wav'
-import Esound from './sounds/E.wav'
-import Asound from './sounds/A.wav'
-import Ssound from './sounds/S.wav'
-import Dsound from './sounds/D.wav'
-import Zsound from './sounds/Z.wav'
-import Xsound from './sounds/X.wav'
-import Csound from './sounds/C.wav'
+import Qsound from '../public/sounds/Q.wav'
+import Wsound from '../public/sounds/W.wav'
+import Esound from '../public/sounds/E.wav'
+import Asound from '../public/sounds/A.wav'
+import Ssound from '../public/sounds/S.wav'
+import Dsound from '../public/sounds/D.wav'
+import Zsound from '../public/sounds/Z.wav'
+import Xsound from '../public/sounds/X.wav'
+import Csound from '../public/sounds/C.wav'
 import Drumpad from './components/Drumpad'
 
 function App() {
   return (
-    <div className="container">
+    <div id="drum-machine" className="container">
       <Drumpad stringID="Q" sound={Qsound}/>
       <Drumpad stringID="W" sound={Wsound}/>
       <Drumpad stringID="E" sound={Esound}/>
