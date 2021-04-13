2a7001a35b59ea3d4d164ff9061316b66910b43c
Toni Virtanen <toniKoodaa@gmail.com>
2021-04-13 13:00:21 +0300

    bug fixing

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,6 +1,7 @@
 
 const Drumpad = ( {stringID, sound}) => {
     console.log("polku -->", sound)
+    console.log("soittimen id -->", stringID)
     let soundEL = document.getElementById(`${stringID}`)
     console.log(soundEL)
 
